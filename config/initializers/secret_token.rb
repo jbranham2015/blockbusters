271b1adeb977673345d1965f7eaf903e9435c72c
vagrant <vagrant@kellogg-dev-box>
2014-04-15 20:12:37 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blockbusters::Application.config.secret_key_base = 'ee65674576900dfeb865e4eda94d09fe43deae216f1126c92d08d7a315fee60123689496531005483d63d92dec3d2035b5297dfb7a16662c68cd0afd9e130a83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blockbusters::Application.config.session_store :cookie_store, key: '_blockbusters_session'
