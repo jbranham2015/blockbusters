30641d47d30ad0f70636eb2d2a6d1d5ad6a52143
vagrant <vagrant@kellogg-dev-box>
2014-04-18 14:42:31 +0000

    how to add movies and dates to the page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417012000) do
+ActiveRecord::Schema.define(version: 20140418141420) do
 
   create_table "books", force: true do |t|
     t.string  "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140417012000) do
     t.string  "director"
     t.integer "price"
     t.text    "actor_bio"
+    t.integer "date"
   end
 
 end
